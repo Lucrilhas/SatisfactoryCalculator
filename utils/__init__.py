87f7d9a17940907f726776db456759306a76487c
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2024-11-01 18:32:53 -0300

    add primary_itens

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,20 @@
 from .GetLogger import logger
 
 
+primary_itens = (
+    "Nitrogen Gas",
+    "Excited Photonic Matter",
+    "Bauxite",
+    "Caterium Ore",
+    "Coal",
+    "Copper Ore",
+    "Iron Ore",
+    "Limestone",
+    "Raw Quartz",
+    "Sulfur"
+)
+
 __all__ = [
     "logger",
+    "primary_itens"
 ]
