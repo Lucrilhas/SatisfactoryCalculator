fb7e63add39fd00537ef9b48abd2401a73bcbfe4
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2024-11-02 11:42:10 -0300

    get title and img

--- getItensData.py
+++ getItensData.py
@@ -46,9 +46,6 @@ def scrape_wiki_item_crafing(url: str) -> dict:
     soup = BeautifulSoup(response.content, "html.parser")
 
     trs = soup.find("h3", string="Crafting").find_next_sibling("table").find("tbody").find_all("tr")
-    if trs is None:
-        return None
-
     trs.pop(0)
 
     crafts = {}
@@ -59,8 +56,6 @@ def scrape_wiki_item_crafing(url: str) -> dict:
         produced_in = tds[2].find("a").text
         prods = tds[3]
 
-        # logger.info(f"recipe: {recipe}")
-        # logger.info(f"produced_in: {produced_in}")
         alt = "Alternate" in recipe
         if alt:
             recipe = recipe[:-9]
@@ -75,7 +70,33 @@ def scrape_wiki_item_crafing(url: str) -> dict:
     return crafts
 
 
+def scrape_title_img_item(url:str, main_page_link:str) -> str:
+    # logger.info(url)
+    response = requests.get(url)
+    response.raise_for_status()
+    # logger.debug(response)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    title = soup.find('span', class_='mw-page-title-main').text
+    img_url = soup.find('img', class_='pi-image-thumbnail').get('src')
+    img_url = main_page_link + img_url
+
+    # logger.info(title)
+    # logger.info(img_url)
+
+    filename = os.path.join(r"data/imgs/" + title + r".png")
+    response = requests.get(img_url, stream=True)
+    response.raise_for_status()
+    with open(filename, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+    return title
+
 if __name__ == "__main__":
+    os.makedirs(r"data", exist_ok=True)
+    os.makedirs(r"data/imgs", exist_ok=True)
+    
     load_dotenv()
     main_page_link = os.getenv("MAIN_PAGE")
     all_itens_url = main_page_link + os.getenv("ALL_ITENS_PAGE")
@@ -88,11 +109,13 @@ if __name__ == "__main__":
             item_name = item_link[6:]
             if item_name in primary_itens:
                 logger.warning(item_name)
+                title = scrape_title_img_item(main_page_link + item_link, main_page_link)
             elif item_name in blacklist:
                 logger.error(item_name)
             else:
                 logger.debug(item_name)
-                all_crafts[item_name] = scrape_wiki_item_crafing(main_page_link + item_link)
+                title = scrape_title_img_item(main_page_link + item_link, main_page_link)
+                all_crafts[title] = {'recipes': scrape_wiki_item_crafing(main_page_link + item_link)}
 
-        with open('recipes.json', 'w') as f:
+        with open(r'data/recipes.json', 'w') as f:
             json.dump(all_crafts, f, indent=4)
