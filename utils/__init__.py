9c236dd56ed5fe93e896410be87918077bef336b
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2024-11-04 08:51:44 -0300

    add blacklist

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,10 +2,12 @@
 from .GetLogger import logger
 from .GetMainColor import get_main_color
 from .ReadJson import read_json
+from .BlacklistedItems import blacklist
 
 
 __all__ = [
     "logger",
     "get_main_color",
-    "read_json"
+    "read_json",
+    "blacklist"
 ]
