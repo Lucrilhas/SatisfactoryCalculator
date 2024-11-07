d31aaf56412205ef954673815ea04381ec58c731
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2024-11-07 15:10:03 -0300

    Funcionando

--- Calculate.py
+++ Calculate.py
@@ -1,29 +1,29 @@
 import networkx as nx
 from utils import *
+import copy
 
 
-def get_first_non_primary(leaves, primarys=primary_clean_items):
+def get_first_non_primary(leaves, primarys):
     for l in leaves:
         if l not in primarys:
             return l
     return False
 
 
-def verify_stop(leaves, primarys=primary_clean_items, max_iteration=51):
+def any_leaf_non_primary(leaves, primarys, do_print=True):
     logger.debug("Folhas: \t\t" + str(leaves))
     logger.debug("Primarios: \t" + str([True if l in primarys else False for l in leaves ]))
     r = any(l not in primarys for l in leaves)
-    if r:
-        logger.info("Ainda existem folhas não primarias --> Continuando Loop!")
-    else:
-        logger.warning("NÃO existem folhas não primarias --> PARANDO Loop!")
+    if do_print:
+        if r:
+            logger.info("Ainda existem folhas não primarias --> Continuando Loop!")
+        else:
+            logger.warning("NÃO existem folhas não primarias --> PARANDO Loop!")
         
     return r
 
-cool_itens = ["Wire", "Rotor", "Reinforced Iron Plate", "Cable", "Motor", "Copper Sheet"]
-def calculate_all_options(target_item=cool_itens[5], draw=False, max_iteration= 51):
+def calculate_all_options(target_item, all_recipes, primarys=primary_clean_items, max_iteration=51):
     ppp = 0
-    all_recipes = read_json("jsons/recipes.json")
     if target_item not in all_recipes.keys():
         logger.error("Item não reconhecido: " + str(target_item))
         exit()
@@ -31,8 +31,8 @@ def calculate_all_options(target_item=cool_itens[5], draw=False, max_iteration= 
     graph = nx.DiGraph()
     graph.add_node(target_item)
 
-    while verify_stop(get_leaves(graph)):
-        folha = get_first_non_primary(get_leaves(graph))
+    while any_leaf_non_primary(get_leaves(graph), primarys):
+        folha = get_first_non_primary(get_leaves(graph), primarys)
 
         logger.info("Processando folha: " + str(folha))
 
@@ -56,8 +56,133 @@ def calculate_all_options(target_item=cool_itens[5], draw=False, max_iteration= 
             break
         ppp +=1
 
-    if draw:
-        colors, label_colors = get_colors(graph, all_recipes, do_label=True)
-        desenha_grafo(graph, colors)
     return graph
 
+
+def add_neighboors_based_on_other(main_graph, info_graph, node):
+    # preds = list(info_graph.predecessors(node))
+    # succs = list(info_graph.successors(node))
+    for pred in info_graph.predecessors(node):
+        main_graph.add_node(pred)
+        main_graph.add_edge(pred, node)
+
+    for suc in info_graph.successors(node):
+        main_graph.add_node(suc)
+        main_graph.add_edge(node, suc)
+
+        # for suc_of_suc in info_graph.successors(node):
+        #     main_graph.add_node(suc_of_suc)
+        #     main_graph.add_edge(suc, suc_of_suc)
+
+
+def calculate_options(target_item, info_graph, recipes, primarys=primary_clean_items, max_iteration=40):
+    ppp=0
+    opcoes = [nx.DiGraph()]
+    opcoes[0].add_node(target_item)
+    # opcoes_recipes = [{}]
+    finais = []
+
+    while opcoes:
+        g = opcoes.pop(0)
+        # opcoes_recipes.pop(0)
+        leaves = get_leaves(g)
+
+        # Finalizar:
+        if not any_leaf_non_primary(leaves, primarys, do_print=False): # All Primary
+            # logger.info("Entrou aqui")
+            # desenha_grafo(g, colors=None)
+            finais.append(g)
+            continue
+
+        # Recipes:
+        sucessors = []
+        for l in leaves:
+            sucessors.append([(l, suc) for suc in info_graph.successors(l)])
+
+        # print("Sucessors: " + str(sucessors))
+        combinations = generate_combinations(sucessors)
+        # print("Combinations: " + str(combinations))
+        for comb in combinations:
+            aux = copy.deepcopy(g)
+            for parent, son in comb:
+                add_neighboors_based_on_other(aux, info_graph, son)
+                if not digraph_exist_on_list(opcoes, aux):
+                    opcoes.append(aux)
+                    # opcoes_recipes.append(recipes_used)
+        # print(combinations)
+
+        if ppp == max_iteration:
+            logger.error("PARADA POR ITERAÇÕES!!")
+            break
+        ppp +=1
+       #  break
+
+    return finais
+    logger.warning(len(finais))
+    desenha_grafo(finais, colors=None)
+        
+        
+    # Iniciar
+    # for suc in options_graph.successors(target_item):
+        
+    #     g.add_node(suc)
+    #     g.add_edge(target_item, suc)
+    #     for suc_of_suc in options_graph.successors(suc):
+    #         g.add_node(suc)
+    #         g.add_node(suc_of_suc)
+    #         g.add_edge(suc, suc_of_suc)
+            
+    #     ops.append(g)
+
+    # ppp = 0
+    # while ops:
+    #     print(len(ops))
+    #     g = ops[0]
+    #     leaves = get_leaves(g)
+    #     print(leaves)
+    #     if not any_leaf_non_primary(leaves, primarys, do_print=False): # All Primary
+    #         print("Entrou aqui")
+    #         desenha_grafo(g, colors=None)
+    #         fins.append(g)
+    #         ops.pop(0)
+    #         continue
+
+    #     # combinations = []
+    #     for l in leaves:
+    #         if l not in primarys:
+    #             logger.warning(l)
+    #             preds = list(options_graph.predecessors(l))
+    #             logger.warning(preds)
+    #             if len(preds) > 0:
+    #                 for pred in preds:
+    #                     g.add_node(pred)
+    #                     g.add_edge(pred, l)
+                        
+    #             succs = list(options_graph.successors(l))
+    #             logger.warning(succs)
+    #             if len(succs) == 1:
+    #                 suc = succs[0]
+    #                 g.add_node(suc)
+    #                 g.add_edge(l, suc)
+    #                 for suc_of_suc in options_graph.successors(suc):
+    #                     g.add_node(suc_of_suc)
+    #                     g.add_edge(suc, suc_of_suc)
+                
+    #             else:
+    #                 pass
+                    
+                    
+    #             # combinations.append(len(list(options_graph.successors(l))))
+
+    #     # print(combinations)
+            
+            
+
+    #     if ppp == max_iteration:
+    #         logger.error("PARADA POR ITERAÇÕES!!")
+    #         break
+    #     ppp +=1
+
+    # for g in fins:
+    #     desenha_grafo(g, colors=None)
+    # desenha_grafo(fins, colors=None)
--- __init__.py
+++ __init__.py
@@ -1,21 +1,84 @@
 # from unused.CalculateTier import define_tiers
 # from unused.CreateGraph import create_graph
-from Calculate import calculate
+from Calculate import calculate_all_options, calculate_options
 from Scrap.ScrapData import get_links_data
 from Scrap.ScrapLinks import save_all_links
-# from unused.presetPlots import preset_mock, preset_mock_plotly_two
-from utils.ReadJson import read_json
 
+# from unused.presetPlots import preset_mock, preset_mock_plotly_two
+from utils import *
+import networkx as nx
 
 if __name__ == "__main__":
     # save_all_links()
     # get_links_data()
     # create_graph()
-    
 
-
-    # recipes = read_json("jsons/recipes.json")
+    recipes = read_json("jsons/recipes.json")
     # preset_mock_plotly_two(recipes)
     # preset_mock(recipes)
-    calculate()
-    pass
+    # cool_itens = ["Wire", "Rotor", "Reinforced Iron Plate", "Cable", "Motor", "Copper Sheet"]
+    # target_items = "Steel Ingot"
+    # target_items = "Steel Pipe"
+    target_items = "Fuel"
+    options_graph = calculate_all_options(target_items, recipes)
+
+    colors = get_colors(options_graph, recipes)
+    desenha_grafo(options_graph, colors)
+    # print("\n====================================\n")
+
+    options = calculate_options(target_items, options_graph, recipes)
+    for o in options:
+        colors = get_colors(o, recipes)
+        desenha_grafo(o, colors)
+        
+
+    # edges = [
+    #     ("A", "Recipe: 0"),
+    #     ("A", "Recipe: 1"),
+    #     ("A", "Recipe: 3"),
+    #     ("A", "Recipe: 4"),
+    #     ("Recipe: 0", "B0"),
+    #     ("Recipe: 0", "B1"),
+    #     ("Recipe: 1", "B1"),
+    #     ("Recipe: 1", "C0"),
+    #     ("C0", "Recipe: 2"),
+    #     ("Recipe: 2", "C1"),
+    #     ("Recipe: 2", "C2"),
+    #     ("Recipe: 3", "D0"),
+    #     ("Recipe: 9", "D1"),
+    #     ("D0", "Recipe: 9"),
+    #     ("D2", "Recipe: 9"),
+    #     ("E", "Recipe: 3"),
+    #     ("Recipe: 4", "F0"),
+    #     ("F0", "Recipe: 5"),
+    #     ("F0", "Recipe: 6"),
+    #     ("Recipe: 5", "F1"),
+    #     ("Recipe: 6", "F2"),
+    #     ("Recipe: 4", "G0"),
+    #     ("G0", "Recipe: 7"),
+    #     ("G0", "Recipe: 8"),
+    #     ("Recipe: 7", "G1"),
+    #     ("Recipe: 8", "G2"),
+    # ]
+    # edges = [
+    #     ("A", "Recipe: 0"),
+    #     ("A", "Recipe: 1"),
+    #     ("Recipe: 0", "B"),
+    #     ("Recipe: 1", "C"),
+    #     ("B", "Recipe: 2"),
+    #     ("B", "Recipe: 3"),
+    #     ("C", "Recipe: 4"),
+    #     ("C", "Recipe: 5"),
+    #     ("Recipe: 2", "D"),
+    #     ("Recipe: 3", "E"),
+    #     ("Recipe: 4", "F"),
+    #     ("Recipe: 5", "G"),
+    #     ("A", "Recipe: 6"),
+    #     ("Recipe: 6", "H"),
+    #     ("H", "Recipe: 7"),
+    #     ("Recipe: 7", "I"),
+    # ]
+    # graph = nx.DiGraph(edges)
+
+    # desenha_grafo(graph, colors=None)
+    # calculate_options("A", graph, recipes)
