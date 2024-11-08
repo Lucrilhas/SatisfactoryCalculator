ab5c147fc0276a0ad529d112402cb3c79eacc7e2
Lucas Cruvinel <LucrilhasBR@hotmail.com>
2024-11-08 10:06:57 -0300

    utils use getenv

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,20 @@
+from dotenv import load_dotenv
+from os import getenv
+
+main_page_link = getenv("MAIN_PAGE")
+items_page_link = main_page_link + getenv("ALL_ITENS_PAGE")
+fluids_page_link = main_page_link + getenv("ALL_FLUIDS_PAGE")
 
 from .GetLogger import logger
 from .GetMainColor import get_main_color
 from .ReadJson import read_json
-from .Constansts import primary_items, primary_items_wiki, blacklist_items, blacklist_items_wiki, blacklist_recipes
+from .Constansts import (
+    primary_items,
+    primary_items_wiki,
+    blacklist_items,
+    blacklist_items_wiki,
+    blacklist_recipes,
+)
 from .GetLeaves import get_leaves
 from .GetColors import get_colors
 from .DesenhaGrafo import desenha_grafo
@@ -11,6 +23,9 @@ from .DiGraphExistOnList import digraph_exist_on_list, recipes_used
 
 
 __all__ = [
+    "main_page_link",
+    "items_page_link",
+    "fluids_page_link",
     "logger",
     "get_main_color",
     "read_json",
@@ -24,5 +39,5 @@ __all__ = [
     "desenha_grafo",
     "generate_combinations",
     "digraph_exist_on_list",
-    "recipes_used"
+    "recipes_used",
 ]
